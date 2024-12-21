1a1f79d8a98a1501815e4e46ebb721f910b7cf2b
Alysson Barrera <alyssonbarrera@outlook.com>
2024-12-21 09:25:12 -0300

    :sparkles: add login bottom sheet view model

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by Alysson Barrera on 21/12/24.
+//
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameLogin: String, password: String) {
+        print(usernameLogin, password)
+        // chamar a camada de back-end ou camada de serviço
+    }
+}
