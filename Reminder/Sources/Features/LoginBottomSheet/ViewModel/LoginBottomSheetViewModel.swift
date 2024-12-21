445ef67e42d67624e8ce857ffeb61a3827de93d6
Alysson Barrera <alyssonbarrera@outlook.com>
2024-12-21 10:28:57 -0300

    :sparkles: add closure

--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -8,13 +8,15 @@
 import Firebase
 
 class LoginBottomSheetViewModel {
+    var successResult: (() -> Void)?
+    
     func doAuth(usernameLogin: String, password: String) {
         print(usernameLogin, password)
         Auth.auth().signIn(withEmail: usernameLogin, password: password) { [weak self] authResult, error in
             if let error = error {
                 print("Falha na autenticação \(error)")
             } else {
-                print("Succeso na autenticação", authResult)
+                self?.successResult?()
             }
         }
     }
